6569851868b2be0c9db7b52ec3e9e2e36241aafa
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-11-03 12:52:02 +0600

    Api Reference-Response Object is Completed

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require("express");
+const handle = require("./handle");
 const app = express();
-app.set("view engine", "ejs");
-
-app
-  .route("/user/admin")
-  .get((req, res) => {
-    res.render("pages/about");
-  })
-  .post((req, res) => {
-    res.send("welcome to page post");
-  })
-  .put((req, res) => {
-    res.send("welcome to page put");
-  });
+const cookieParser = require("cookie-parser");
+app.use(express.json());
+app.use(cookieParser());
+const adminRoute = express.Router();
+adminRoute.get("/dashboard", (req, res) => {
+  console.log(req);
+  res.send("Welcome admin DashBoard");
+});
+app.use("/admin", adminRoute);
+app.get("/user/:id", handle);
+app.post("/user/:id", (req, res) => {
+  console.log(req.route);
+  res.send("Hello World");
+});
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
