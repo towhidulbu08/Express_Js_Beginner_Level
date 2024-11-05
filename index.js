418e57cdf5dba3827a732035a514e034bea33cd1
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-11-05 13:54:51 +0600

    API Reference-Middleware is complete

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const express = require("express");
 const app = express();
-app.set("view engine", "ejs");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+const adminRouter = express.Router();
+
+const loggerWrapper = (options) => {
+  return function (req, res, next) {
+    if (options.log) {
+      console.log(
+        `${new Date(Date.now()).toLocaleString()} - ${req.method} - ${
+          req.originalUrl
+        } - ${req.protocol} - ${req.ip}`
+      );
+      next();
+    } else {
+      throw new Error("Failed Log");
+    }
+  };
+};
+
+adminRouter.use(loggerWrapper({ log: false }));
+adminRouter.get("/dashboard", (req, res) => {
+  res.send("DashBoard");
+});
+app.use("/admin", adminRouter);
+
 app.get("/about", (req, res) => {
-  res.set("Title", "Learn With Sumit");
-  console.log(res.get("Title"));
-  res.end();
+  console.log("About");
+  res.send("About");
 });
-app.get("/test", (req, res) => {
-  res.send("hello");
-});
+
+const errorMiddleware = (err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).send("There Was An server side error");
+};
+adminRouter.use(errorMiddleware);
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
